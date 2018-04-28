3ee9378d4f74b6a5cb8c09cdcdd5db113176b8a7
swe <sweswe@nexlabs.co>
2018-04-29 01:04:57 +0630

    add travis.yml file and edit testcase

--- test/test_mm_phonenumbers.rb
+++ test/test_mm_phonenumbers.rb
@@ -3,33 +3,24 @@ require 'mm_phonenumbers'
 
 class MmPhonenumbersTest < Test::Unit::TestCase
   def test_is_valid_phone
-    p "Enter Phone number"
-    phone_number = gets
-    phone_number ||= ''
-    phone_number.chomp!
+    phone_number = "092012345"
     
     assert_equal true, MmPhonenumbers::is_valid_phone(phone_number)
   end
 
   def test_get_telecom_name
-    p "Enter Phone number"
-    phone_number = gets
-    phone_number ||= ''
-    phone_number.chomp!
-
+    # p "Enter Phone number"
+    # phone_number = gets
+    # phone_number ||= ''
+    # phone_number.chomp!
+    phone_number = "092012345"
     assert_equal "MPT", MmPhonenumbers::get_telecom_name(phone_number)
   end
 
   def test_is_provided_telecom
     p "Enter Telecom name"
-    telecom_name = gets
-    telecom_name ||= ''
-    telecom_name.chomp!
-
-    p "Enter Phone number"
-    phone_number = gets
-    phone_number ||= ''
-    phone_number.chomp!
+    telecom_name = "mpt"
+    phone_number = "092012345"
 
     assert_equal true, MmPhonenumbers.is_provided_telecom(
       telecom_name, 
--- test/test_sanitize_phone.rb
+++ test/test_sanitize_phone.rb
@@ -3,28 +3,19 @@ require 'sanitize_phone'
 
 class SanitizePhoneTest < Test::Unit::TestCase
   def test_remove_whitespace_and_dash
-    p "Enter phone to sanitize"
-    phone_number = gets
-    phone_number ||= ''
-    phone_number.chomp!
+    phone_number = "09-20-12345 "
 
     assert_equal '09450012345', SanitizePhone::remove_whitespace_and_dash(phone_number)
   end
 
   def test_remove_double_country_code
-    p "Enter phone to remove double country code"
-    phone_number = gets
-    phone_number ||= ''
-    phone_number.chomp!
+    phone_number = "+959092012345"
 
     assert_equal '+959450012345', SanitizePhone::remove_double_country_code(phone_number)
   end
 
   def test_remove_zero_before_area_code
-    p "Enter phone to remove zero before areacode"
-    phone_number = gets
-    phone_number ||= ''
-    phone_number.chomp!
+    phone_number = "092012345"
 
     assert_equal '+959450012345', SanitizePhone::remove_zero_before_area_code(phone_number)
   end
