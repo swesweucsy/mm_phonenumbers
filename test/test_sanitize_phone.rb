7a00ff7f01f7cd475a118d4c0fdbbcd6e8c71b87
swe <sweswe@nexlabs.co>
2018-04-29 01:13:09 +0630

    edit test case

--- test/test_sanitize_phone.rb
+++ test/test_sanitize_phone.rb
@@ -3,20 +3,20 @@ require 'sanitize_phone'
 
 class SanitizePhoneTest < Test::Unit::TestCase
   def test_remove_whitespace_and_dash
-    phone_number = "09-20-12345 "
-
-    assert_equal '09450012345', SanitizePhone::remove_whitespace_and_dash(phone_number)
+    phone_number = "09-450-012345 "
+    assert_equal '09450012345', 
+      SanitizePhone::remove_whitespace_and_dash(phone_number)
   end
 
   def test_remove_double_country_code
-    phone_number = "+959092012345"
-
-    assert_equal '+959450012345', SanitizePhone::remove_double_country_code(phone_number)
+    phone_number = "+9509450012345"
+    assert_equal '+959450012345', 
+      SanitizePhone::remove_double_country_code(phone_number)
   end
 
   def test_remove_zero_before_area_code
-    phone_number = "092012345"
-
-    assert_equal '+959450012345', SanitizePhone::remove_zero_before_area_code(phone_number)
+    phone_number = "+9509450012345"
+    assert_equal '+959450012345', 
+      SanitizePhone::remove_zero_before_area_code(phone_number)
   end
 end
